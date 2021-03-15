99b8583549cc53f7ef1bec52582814d11b4612d1
delight123123 <fhdhaldh@naver.com>
2021-03-15 19:39:42 +0900

    설정 중 확인해야할 설정들 파일업로드 메일 발송 인터셉터

---
+++ singelbyoracle/src/main/java/com/single/board/SingelApplication.java
@@ -0,0 +1,13 @@
+package com.single.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingelApplication.class, args);
+	}
+
+}
---
+++ singelbyoracle/src/main/java/com/single/board/common/FileuploadProperties.java
@@ -0,0 +1,8 @@
+package com.single.board.common;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileuploadProperties {
+
+}
