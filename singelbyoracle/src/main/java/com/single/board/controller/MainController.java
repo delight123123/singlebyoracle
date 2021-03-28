d8098b2c8fe738ffcc1cbbdfd05f9934a39ff9a5
delight123123 <fhdhaldh@naver.com>
2021-03-28 15:14:01 +0900

    asdasdqqq

--- singelbyoracle/src/main/java/com/single/board/controller/MainController.java
+++ singelbyoracle/src/main/java/com/single/board/controller/MainController.java
@@ -1,13 +1,58 @@
 package com.single.board.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.single.board.common.PaginationInfo;
+import com.single.board.common.Utility;
+import com.single.board.model.ReboardVO;
 
 import lombok.extern.java.Log;
 
 @Log
 @Controller
 public class MainController {
-
 	
+	private final static Logger logger=LoggerFactory.getLogger(MainController.class);
+
+	@Autowired
+	private ReboardService reboardService;
+	
+	@RequestMapping("/main")
+	public Object Mainhome(@ModelAttribute ReboardVO reboardVo,Model model) {
+		logger.info("메인화면 보이기");
+		logger.info("목록 파라미터 reboardVo={}",reboardVo);
+		
+		PaginationInfo pagingInfo=new PaginationInfo();
+		pagingInfo.setBlockSize(Utility.BLOCK_SIZE);
+		pagingInfo.setRecordCountPerPage(Utility.RECORD_COUNT);
+		pagingInfo.setCurrentPage(reboardVo.getCurrentPage());
+		
+		reboardVo.setRecordCountPerPage(Utility.RECORD_COUNT);
+		reboardVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
+		
+		logger.info("셋팅후 reboardVo={}",reboardVo);
+		
+		List<ReboardVO> list=reboardService.reboardSearch(reboardVo);
+		logger.info("검색결과 list.size()={}",list.size());
+		
+		int totalRecord=reboardService.searchTotal(reboardVo);
+		logger.info("totalRecord={}",totalRecord);
+		
+		pagingInfo.setTotalRecord(totalRecord);
+		
+		logger.info("pagingInfo={}",pagingInfo);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("pagingInfo", pagingInfo);
+		
+		return "Main";
+	}
 }
---
+++ singelbyoracle/src/main/java/com/single/board/model/UpfileListVO.java
@@ -0,0 +1,17 @@
+package com.single.board.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpfileListVO {
+	private int fileNo;
+	private String fileName;
+	private int filesize;
+	private int downCount;
+	private String originalFileName;
+	private int reboardNo;
+}
