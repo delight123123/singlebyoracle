49329463ae38fb42ccb2f40a6799750ff1d271c7
delight123123 <fhdhaldh@naver.com>
2021-04-06 19:19:06 +0900

    2021-04-06 커밋

---
+++ singelbyoracle/src/main/java/com/single/board/controller/ReboardController.java
@@ -0,0 +1,23 @@
+package com.single.board.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ReboardController {
+
+	private final static Logger logger=LoggerFactory.getLogger(ReboardController.class);
+	
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public Object write() {
+		
+		logger.info("글쓰기 화면 보이기");
+		
+		
+		return "reboard/write";
+	}
+	
+}
