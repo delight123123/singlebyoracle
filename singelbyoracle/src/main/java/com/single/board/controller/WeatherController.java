233caf15286a2d185ea9bc9ef071b389bf5f403a
Administrator <Administrator@61.75.81.236>
2021-04-13 17:46:12 +0900

    2021-04-13 망할 지도때문에 하루가 갔다

--- singelbyoracle/src/main/java/com/single/board/controller/WeatherController.java
+++ singelbyoracle/src/main/java/com/single/board/controller/WeatherController.java
@@ -3,12 +3,23 @@ package com.single.board.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WeatherController {
 
 	private final static Logger logger=LoggerFactory.getLogger(WeatherController.class);
 	
-	
+	@RequestMapping("/weather")
+	public Object weather(@RequestParam String xx,@RequestParam String yy,Model model) {
+		logger.info("일기예보 화면 보이기");
+		
+		model.addAttribute("xx", xx);
+		model.addAttribute("yy", yy);
+		
+		return "weather/weatherForecast";
+	}
 	
 }
