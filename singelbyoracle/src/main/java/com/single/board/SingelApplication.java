b25f6435c2ddfa15d1ac59c6975949efeadae92f
delight123123 <fhdhaldh@naver.com>
2021-04-25 23:02:21 +0900

    2021-04-25 집 스프링부트로 만드는 것은 끝났으나 우분투에 올리는데 한계를 느낌

--- singelbyoracle/src/main/java/com/single/board/SingelApplication.java
+++ singelbyoracle/src/main/java/com/single/board/SingelApplication.java
@@ -2,13 +2,20 @@ package com.single.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.single.board.common.FileuploadProperties;
 
 @EnableConfigurationProperties({FileuploadProperties.class})
 @SpringBootApplication
-public class SingelApplication {
+public class SingelApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SingelApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SingelApplication.class, args);
